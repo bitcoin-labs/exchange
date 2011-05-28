f37ba8d3c66c7d027a3d528fe2b2949e86b8c708
Sheldon Thomas <swthomas@stmbpro.local>
2011-05-28 18:45:59 -0400

    added validation to order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+  validates :amount, :presence => true
+  validates :price, :presence => true
+  validates :type, :presence => true
+  validates :user_id, :presence => true
+  validates :created_at, :presence => true
 end
