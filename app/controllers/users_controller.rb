a167bc3e9046c4614d0cbe35864bffb2ceca8016
Andrew Schaaf <andrew@andrewschaaf.com>
2011-05-28 16:46:32 -0400

    some stub pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class SiteController < ApplicationController
+  
+  def login
+    respond_to {|format| format.html}
+  end
+  
+  def register
+    respond_to {|format| format.html}
+  end
+  
+end
